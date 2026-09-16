0d1be235626e015bac14d2a4d561375560c01bc2
SkylaChen <zc3788@nyu.edu>
2026-09-16 03:46:43 -0400

    push

--- week2/sketch2.js
+++ week2/sketch2.js
@@ -1,29 +1,32 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100);
+  background(100)
 
   fill(100,0,0)
-  strokeWeight(15)
-  stroke(0,0,100)
+  //noStroke()
+  strokeWeight(3)
+  stroke(200,100,0)
  
  
-};
-function draw() {  
+}
+function draw(){  
 
+}
 
- }
- function mousedragged() {
-   line(pmouseX, pmouseY,mouseX, mouseY);
-
+function mouseDragged() {
+//strokeWeight(random(1, 10));
+  //let s = map(mouseX, 0, width, 1, 15);
+  //strokeWeight(s);
+   //line(pmouseX,pmouseY,mouseX,mouseY)
       //background(100);
+let v = dist(pmouseX, pmouseY, mouseX, mouseY);
+  strokeWeight(v);
+  line(pmouseX, pmouseY, mouseX, mouseY);
 
- }
+}
  function mousePressed() {
-    background(100);
+    //background(100);
 }
 function keyPressed() {
-    background(100);
-
-
-
+      background(100);
 }
